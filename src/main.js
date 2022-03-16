1db42e43c2142d01aee92124e6daa2345f63086e
wujian <wujian@gobestsoft.com>
2022-03-16 23:41:39 +0800

    按需引入antdv

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { store } from './vuex/index'
-import { Button } from 'ant-design-vue'
+import { Button, message } from 'ant-design-vue'
 
 const app = createApp(App)
+app.config.productionTip = false;
 
 app.use(router)
 .use(store)
 .use(Button)
 .mount('#app')
+
+app.config.globalProperties.$message = message;
